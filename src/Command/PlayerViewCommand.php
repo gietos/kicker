90f6b1af4f6142a7fb08b86cce7e3b6d7e8459c4
Sergey Kasatkin <spam@onsky.ru>
2017-08-15 14:37:30 +0300

    games count

--- src/Command/PlayerViewCommand.php
+++ src/Command/PlayerViewCommand.php
@@ -4,6 +4,9 @@ namespace Gietos\Kicker\Command;
 
 use Gietos\Kicker\Component\View;
 use Gietos\Kicker\Model\Player;
+use Gietos\Kicker\Service\Game;
+use Moserware\Skills\GameInfo;
+use Moserware\Skills\TrueSkill\FactorGraphTrueSkillCalculator;
 
 class PlayerViewCommand extends AbstractCommand
 {
@@ -11,8 +14,18 @@ class PlayerViewCommand extends AbstractCommand
     {
         $id = $parameters['id'];
 
+        /** @var Player $player */
         $player = $this->entityManager->find(Player::class, $id);
 
-        return $this->render('player/view.html.twig', compact('player'));
+        $game = new Game($this->entityManager, new FactorGraphTrueSkillCalculator, new GameInfo);
+        $wins = $game->getWins($player);
+        $losses = $game->getLosses($player);
+        $gamesCount = $wins + $losses;
+        $winRate = null;
+        if ($wins != 0 && $gamesCount != 0) {
+            $winRate = $wins / $gamesCount * 100;
+        }
+
+        return $this->render('player/view.html.twig', compact('player', 'gamesCount', 'winRate'));
     }
 }

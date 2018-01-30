ca6e290eb7b139bd1367eab81419db0326b75318
Sergey Kasatkin <s.kasatkin@admitad.com>
2018-01-30 18:34:11 +0300

    Whoops for errors

--- bootstrap.php
+++ bootstrap.php
@@ -5,12 +5,18 @@ date_default_timezone_set('Europe/Moscow');
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 use Dotenv\Dotenv;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (!file_exists(__DIR__ . '/.env')) {
     throw new \Exception('Environment file was not found');
 }
+
+$whoops = new Run;
+$whoops->pushHandler(new PrettyPageHandler);
+$whoops->register();
 
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
